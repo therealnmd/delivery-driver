413bdb5761378d45605e413821d56a60f841d70c
Nguyen Manh Duc <ducnm203@gmail.com>
2024-11-07 10:57:37 +0700

    added new files

---
+++ Assets/Delivery.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    //[SerializeField] Color32 hasPackageColor = new Color32(102,97,255,255);
+    //[SerializeField] Color32 noPackageColor = new Color32(102, 97, 255, 255);
+    [SerializeField] float timeToVanish = 0.2f;
+    bool isTaken;
+
+    SpriteRenderer carSpriteRenderer;
+
+    void Start()
+    {
+        carSpriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("You're hitting something");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Package" && isTaken == false)//khi xe k có hàng đi qua và trigger nhận hàng ==> xe đã có hàng
+        {
+            Debug.Log("Package captured");
+            isTaken = true; //khi đi qua object có tag "Package", xe trigger nhận hàng và điều kiện isTaken đúng
+            //spriteRenderer.color = hasPackageColor;
+            SpriteRenderer boxSpriteRenderer = collision.GetComponent<SpriteRenderer>(); //lấy màu của package 
+            carSpriteRenderer.color = boxSpriteRenderer.color; //gán màu của package cho ô tô
+            Destroy(collision.gameObject, timeToVanish);
+        }    
+        if (collision.tag == "Speed Orb")
+        {
+            Debug.Log("Car is sped up");
+            
+            Destroy(collision.gameObject, timeToVanish);
+        }
+        if (collision.tag == "Customer" && isTaken)
+        {
+            Debug.Log("Package delivered");
+            isTaken = false; //khi đi qua object có tag "Customer" và điều kiện isTaken đúng, xe trigger giao hàng
+                             // và điều kiện isTaken sai ==> sẽ không thể giao lại hàng nếu không có hàng trong xe
+            
+        }
+    }
+}
---
+++ Assets/Driver.cs
@@ -0,0 +1,101 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nếu các số liệu Rotation là + thì sẽ xoay theo chiều kim đồng hồ và ngược lại
+        //Rotation:
+        //trục X sẽ là xoay upside-down (từ trên xuống dưới) theo phương ngang
+        //trục Y sẽ là xoay từ trái sang phải theo trục thẳng đứng
+        //trục Z sẽ là xoay tròn 360 độ theo hướng mắt nhìn thẳng
+        //(nếu số liệu là +, lấy điểm đặt mắt ở 0 độ thì object sẽ quay ngược chiều kim đồng hồ)
+        //(nếu số liệu là -, lấy điểm đặt mắt ở 180 độ thì object sẽ quay thuận chiều kim đồng hồ)
+        //transform.Rotate(0, 0, 90);
+
+        //Scale:
+        //trục X sẽ là kéo rộng bề mặt object theo phương ngang
+        //trục Y sẽ là kéo dài bề mặt object theo trục thẳng đứng
+        //trục Z sẽ là kéo rộng bề mặt object theo hướng mắt nhìn thẳng
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Translate:
+        //trục X sẽ là di chuyển lên xuống theo phương ngang
+        //trục Y sẽ là di chuyển trái phải theo phương thẳng đứng
+        //trục Z sẽ là di chuyển tiến tới/lùi về theo hướng mắt nhìn thẳng
+        //transform.Rotate(0, 0, -0.1f);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveSpeed, 0);
+        
+    }
+}
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = -40.5f;
+    [SerializeField] float normalSpeed = 2.6f;
+    [SerializeField] float slowSpeed = 0.1f;
+    [SerializeField] float boostSpeed = 7.0f;
+    float currentSpeed;
+    
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * normalSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, steerAmount);
+        transform.Translate(0, moveAmount, 0);
+        
+        
+        
+      
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        normalSpeed = slowSpeed;
+        //StartCoroutine(SlowSpeed());
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Speed Orb")
+        {
+            normalSpeed = boostSpeed;
+            //StartCoroutine(BoostSpeed());           
+        }    
+        
+
+    }
+    /*private IEnumerator BoostSpeed()
+    {
+        currentSpeed = boostSpeed;
+        yield return new WaitForSeconds(3);
+        currentSpeed = normalSpeed;
+    }
+
+    private IEnumerator SlowSpeed()
+    {
+        currentSpeed = slowSpeed;
+        yield return new WaitForSeconds(3);
+        currentSpeed = normalSpeed;
+    }*/
+}
